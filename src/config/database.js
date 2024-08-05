90cda6450db696141b5356651862fff3bf453f90
Gabriel Lopes <Gabrielcostaplay@gmail.com>
2024-08-05 20:37:26 -0300

    modified:   src/config/database.js

--- src/config/database.js
+++ src/config/database.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 
 module.exports = {
   dialect: 'mariadb',
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE,
+  host: process.env.PGHOST,
+  username: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  endPoint: process.env.ENDPOINT_ID,
   define: {
     timestamps: true,
     underscored: true,
