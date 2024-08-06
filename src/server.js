56485a735902a76de9c8f768afc12235a6c4aaa1
Gabriel Lopes <Gabrielcostaplay@gmail.com>
2024-08-06 09:30:35 -0300

    modified:   src/config/appConfig.js 	modified:   src/server.js

--- src/server.js
+++ src/server.js
@@ -7,7 +7,6 @@ const port = process.env.PORT || 9001;
 app.listen(port, () => {
   // Importa uma linha em branco
   console.log();
-
   // Exibe uma mensagem informando que o servidor está escutando na porta 3000
   // e inclui um link para a página inicial
   console.log(`Escutando na porta ${port}, clique: http://localhost:${port}`);
